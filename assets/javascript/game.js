090206bf223e6e164fda01e0488f99f0cff85ff8
Jessica Nguyen <jessicanguyen320@gmail.com>
2018-11-08 23:20:44 -0800

    finished hw

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -18,8 +18,7 @@ console.log("The secret letter is: " + secretLetter);
 
 // At page load, we initialize win, losses, guessesLeft & lettersGuessed variables
 var win = 0;
-var losses = 0;
-var guessesLeft = 9;
+var lost = 0;
 var lettersGuessed = [];
 
 // Create a counter track how many times pressed
@@ -32,9 +31,20 @@ var guesses = document.getElementById("guesses");
 var guessesLeft = document.getElementById("guesses-left");
 var wins = document.getElementById("wins");
 var losses = document.getElementById("losses");
+var startButton = document.getElementById("start-button");
+var playAgain = document.getElementById("play-again-button");
 
 // Every time we click the button, the game will start and re-initialize the variables declared at page load (resetting the game)
 function playButton() {
+
+    // On play button press, hide the startButton
+
+    startButton.style.visibility = "hidden";
+    playAgain.style.visibility = "hidden";
+
+
+    // Initiate 9 guesses
+    var triesLeft = 9;
 
     // Remove the question mark
     guesses.innerHTML = "";
@@ -56,25 +66,44 @@ function playButton() {
         // if letters do not match, add that letter to "guesses" element
         if (event.key !== secretLetter) {
 
+            // add event.key in between <div id="guesses"></div> 
 
-            // Ever time we press a key, we add the letter to the array below
-            lettersGuessed.push(event.key);
+            // If guesses left do not = 0, keep looping
+            if (triesLeft !== 1) {
 
-            guesses.innerText = lettersGuessed[counter] // Inner text of #guesses = letterGuessed[position of array]
+                guesses.appendChild(document.createTextNode(event.key));
 
-            // after we add the first position to the inner text, next time we press, we want the next position
-            counter++
 
+                triesLeft--;
+
+                // guesses left is the element with the number of guesses left
+                guessesLeft.innerText = triesLeft;
+            } else {
+
+                // add one to the lost
+                lost++
+                guessesLeft.innerText = 0;
+
+                // change the innerText to value of lost
+                losses.innerText = lost;
+
+                // Hide "press to play button"
+                startButton.style.visibility = "hidden";
+                // Show "play again button:
+                playAgain.style.visibility = "visible";
+
+                // Stop the onkeyup event since we lost!
+                document.onkeyup = null;
+            }
+
+        } else {
+            win++;
+            wins.innerText = win;
+            // Stop the onkeyup event since we won!
+            document.onkeyup = null;
+            // Show "play again button:
+            playAgain.style.visibility = "visible";
         }
 
     }
-}
-
-
-
-
-
-
-function guessLetter() {
-
 }
